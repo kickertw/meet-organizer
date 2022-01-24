b7bcceb3de6b7ff92e67770c68dcc5faa731e107
Tim Wong <k1ck3rtw@gmail.com>
2022-01-24 08:23:02 -0500

    login fixed, table component started

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginView from './views/login';
+import EventTable from './components/event-table';
+import { Button } from 'react-bootstrap';
 // import { Link, Switch } from 'react-router-dom';
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
-function App() {
-  return (
-    <LoginView />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //     <div>
-    //       <Link to="/test">Test</Link> | &nbsp; <Link to="/test2">Test2</Link>
-    //     </div>
-    //   </header>
-    // </div>
-  );
+function App() {    
+    const [isAuth, setAuth] = useState(false);
+    const [role, setRole] = useState('');
+
+    useEffect(() => {
+        setAuth(localStorage.getItem('Password') != null);
+        setRole(localStorage.getItem('Role'))
+    },[]);
+
+    const updateLogin = (isLoggedIn, userRole) => {
+        setAuth(isLoggedIn);
+        if (isLoggedIn) {
+            setRole(userRole);
+        }
+    }
+
+    const logout = () => {
+        localStorage.removeItem('Password');
+        localStorage.removeItem('Role');
+        setAuth(null);
+        setRole('');
+    }
+
+    if (!isAuth) {
+        return (<LoginView updateLoginCallback={updateLogin} />);
+    } else {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <Button variant='secondary' onClick={logout} >Logout</Button>
+                </header>
+                <EventTable />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/views/login.jsx
+++ src/views/login.jsx
@@ -2,30 +2,43 @@ import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import styles from './login.module.scss';
 
-const LoginView = () => {
-  const [pass, setPass] = useState('');
+const LoginView = ({updateLoginCallback}) => {
+    const [pass, setPass] = useState('');
 
-  const checkPass = (e) => {
-    if (pass === process.env.REACT_APP_ADMIN_PASS) {
-        alert('Admin');
-    } else if (pass === process.env.REACT_APP_READ_ONLY_PASS) {
-        alert('Read Only');
-    } else {
-        alert('❌❌❌');
+    const checkPass = (e) => {
+        if (pass === process.env.REACT_APP_ADMIN_PASS) {
+            localStorage.setItem('Password', pass);
+            updateLoginCallback(true, 'admin');
+        } else if (pass === process.env.REACT_APP_READ_ONLY_PASS) {
+            localStorage.setItem('Password', pass);
+            updateLoginCallback(true, 'view');
+        } else {
+            alert('❌❌❌');
+        }
+    };
+
+    const clearStorage = () => {
+        localStorage.removeItem('Password');
+        updateLoginCallback(false);
     }
-  };
 
-  return (
-    <div className={styles.LoginView} data-testid="LoginView">
-        <input
-            type="password"
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-            aria-label="password"
-        />
-        <Button variant="primary" onClick={checkPass}>Go</Button>
-    </div>
-  );
+    return (
+        <div className={styles.LoginContainer}>
+            <div className={styles.LoginForm} data-testid="LoginView">
+                <img src="http://sonshinegymnastics.com/wp-content/themes/tnc-ss-gym/images/logo/logo.png" />
+                <input
+                    type="password"
+                    className={styles.inputs}
+                    value={pass}
+                    onChange={(e) => setPass(e.target.value)}
+                    aria-label="password"
+                />
+                <Button variant="primary" onClick={checkPass} className={styles.inputs}>Go</Button>
+                <Button variant="secondary" onClick={clearStorage} className={styles.inputs}>Clear</Button>
+                <p>{localStorage.getItem('Password')}</p>
+            </div>
+        </div>
+    );
 };
 
 export default LoginView;
